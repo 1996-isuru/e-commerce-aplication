b95eec111f07008da7e5d94ff33af33c3a2a49c2
1996-isuru <isurusrimalnt@gmail.com>
2021-05-13 23:24:15 +0530

    routing all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrderListComponent } from './orders/order-list/order-list.component';
+import { CreateProductComponent } from './products/create-product/create-product.component';
 import { ViewAllProductComponent } from './products/view-all-product/view-all-product.component';
 
 
@@ -8,7 +9,8 @@ const routes: Routes = [
   { path: 'products', loadChildren: () => import('./products/productds.module').then(m => m.ProductdsModule) },
   { path: 'orders', component: OrderListComponent },
   { path: 'view-product', component: ViewAllProductComponent },
-  { path: 'list-product', component: ViewAllProductComponent }
+  { path: 'list-product', component: ViewAllProductComponent },
+  { path: 'create-product', component: CreateProductComponent }
 ];
 
 
